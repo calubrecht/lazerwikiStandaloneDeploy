0a037e671547f6d6c20b56aaa2a5ae80b1f604f8
Chad Lubrecht <chad@calubrecht.us>
2025-05-04 16:35:04 -0400

    Control file permissions.

--- install.sh
+++ install.sh
@@ -13,6 +13,9 @@ adduser --system --group lazerwiki
 
 cp -R lazerwiki.sh lazerwiki.jar lazerwiki.db lib /opt/lazerwiki
 chown -R lazerwiki:lazerwiki /opt/lazerwiki
+chmod -R 664 /opt/lazerwiki/*
+chmod -R +X /opt/lazerwiki/*
+chmod 660 /opt/lazerwiki/lazerwiki.db
 
 ./createAdminUser
 
